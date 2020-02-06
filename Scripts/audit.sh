faf1c168f0a796fd908fb7d96bdd1b17fa27295b
butterf1i <32023353+butterf1i@users.noreply.github.com>
2020-02-06 17:33:09 -0600

    begin firewall checking

--- Scripts/audit.sh
+++ Scripts/audit.sh
@@ -4,6 +4,8 @@
 cd ~
 
 # check for stray ssh keys
+echo "SSH:"
+echo "===="
 if test -f /etc/ssh/sshd_config
 then
 	AuthKeys=$(fgrep -m 1 AuthorizedKeysFile /etc/ssh/sshd_config)
@@ -22,3 +24,15 @@ then
 	echo -n  "Warning: found SSH key file: "
 	echo $KeyLocation
 fi
+
+
+echo "\n\n"
+
+
+# check firewall rules
+InputRules=$(iptables -L INPUT)
+ForwardRules=$(iptables -L FORWARD)
+OutputRules=$(iptables -L OUTPUT)
+echo "FIREWALL RULES:"
+echo "==============="
+echo "$InputRules \n $ForwardRules \n $OutputRules \n"
